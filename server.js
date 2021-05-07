1757b421142e3e16b21f6ec064cd6d5633f52fbb
unknown <amal.mans556@gmail.com>
2021-05-07 01:23:55 +0100

    last comm

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const path = require('path')
 
 const app = express();
 app.use(express.json());
@@ -37,13 +38,22 @@ mongoose.connect(
     console.log("Connected to MongoDB atlas");
   }
 );
+// serve static assets if in production 
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
-app.get("/", (req, res) => {
-  res.send({ msg: "wellcome to my app" });
-});
-//app.get('/error',(req,res) => res.send(error))
+
+//app.get("/", (req, res) => {
+//  res.send({ msg: "wellcome to my app" });
+//});
+////app.get('/error',(req,res) => res.send(error))
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-  console.log("Server is running on port http://localehost", PORT);
+  console.log("Server is running on port http://localhost", PORT);
 });
